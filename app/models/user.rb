7c31e1b5331d7fcd633e887144a2e3e99278b85c
Valentina <vale.gutierrezc@gmail.com>
2019-05-02 12:54:46 -0400

    facebook users can now save avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,12 @@ class User < ApplicationRecord
   has_many :messages_sent, class_name: "Message", foreign_key: "sender_id"
   has_many :messages_received, class_name: "Message", foreign_key: "receiver_id"
 
+  require 'open-uri'
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      downloaded_image = open(auth.info.image)
+      user.image.attach(io: downloaded_image, filename: 'avatar.jpg', content_type: downloaded_image.content_type)
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
