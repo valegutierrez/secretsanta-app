d3f606daa87be8f1c7a0e4b55afea8ae14f01ef3
Valentina <vale.gutierrezc@gmail.com>
2019-05-02 20:18:07 -0400

    cleaned css in app

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,8 @@
 class ConversationsController < ApplicationController
   def index
     @conversations = Conversation.all
+    @notifications = current_user.notifications
+    @notifications.update_all checked: true
   end
 
   def show
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,4 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
-    @notifications.update_all checked: true
   end
 end
